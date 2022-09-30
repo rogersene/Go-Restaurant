bf92544a1b67d5a8ee42c51be9c1251fc2f7e98f
Roger Crescencio <rogersenefaria@gmail.com>
2022-09-29 22:18:38 -0300

    refactor: converted modalAppFood component to typescript

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,30 +1,27 @@
-import { Component, createRef } from 'react';
+import { useRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
 
-class ModalAddFood extends Component {
-  constructor(props) {
-    super(props);
+interface newFoodFormProps {
+  image: string;
+  name: string;
+  price: string;
+}
 
-    this.formRef = createRef();
-  }
+function ModalAddFood({setIsOpen,isOpen, handleAddFood }) {
+  const formRef = useRef();
 
-  handleSubmit = async data => {
-    const { setIsOpen, handleAddFood } = this.props;
-
+  const handleSubmit = async (data: newFoodFormProps) => {
     handleAddFood(data);
     setIsOpen();
   };
 
-  render() {
-    const { isOpen, setIsOpen } = this.props;
-
     return (
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Novo Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -41,7 +38,6 @@ class ModalAddFood extends Component {
         </Form>
       </Modal>
     );
-  }
 };
 
 export default ModalAddFood;
